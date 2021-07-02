94bfa936ad512af3a7186e93837c3ac03fcefff8
taishikato <taishi.k0903@gmail.com>
2021-07-01 23:31:38 -0700

    fix: fix social icon size

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -5,22 +5,22 @@ import {faLinkedin, faMedium, faDev, faGithub, faTwitter, faInstagram} from '@fo
 const Socials = () => (
   <>
     <a href="https://www.linkedin.com/in/takato0903/" target="_blank" className="linkedin px-2">
-      <FontAwesomeIcon icon={faLinkedin} size="xs" className="h-12 w-12" />
+      <FontAwesomeIcon icon={faLinkedin} size="2x" className="h-12 w-12" />
     </a>
     <a href="https://medium.com/@TaishiKato" target="_blank" className="medium px-2">
-      <FontAwesomeIcon icon={faMedium} size="xs" className="h-12 w-12" />
+      <FontAwesomeIcon icon={faMedium} size="2x" className="h-12 w-12" />
     </a>
     <a href="https://dev.to/taishi" target="_blank" className="medium px-2">
-      <FontAwesomeIcon icon={faDev} size="xs" className="h-12 w-12" />
+      <FontAwesomeIcon icon={faDev} size="2x" className="h-12 w-12" />
     </a>
     <a href="https://github.com/taishikato" target="_blank" className="github px-2">
-      <FontAwesomeIcon icon={faGithub} size="xs" className="h-12 w-12" />
+      <FontAwesomeIcon icon={faGithub} size="2x" className="h-12 w-12" />
     </a>
     <a href="https://twitter.com/hyped_sushi" target="_blank" className="twitter px-2">
-      <FontAwesomeIcon icon={faTwitter} size="xs" className="h-12 w-12" />
+      <FontAwesomeIcon icon={faTwitter} size="2x" className="h-12 w-12" />
     </a>
     <a href="https://www.instagram.com/hyped_sushi/" target="_blank" className="instagram px-2">
-      <FontAwesomeIcon icon={faInstagram} size="xs" className="h-12 w-12" />
+      <FontAwesomeIcon icon={faInstagram} size="2x" className="h-12 w-12" />
     </a>
     <style jsx>{`
       .linkedin {
