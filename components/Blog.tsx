2e80a47b2cca837b47ab57c743dcd3f95e221c3f
taishikato <taishi.k0903@gmail.com>
2022-02-06 18:32:16 +0900

    fix warning

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -6,6 +6,7 @@ interface IPost {
   pubDate: string
   thumbnail: string
   title: string
+  guid: string
 }
 
 interface IProps {
@@ -13,24 +14,24 @@ interface IProps {
 }
 
 const Blog: FC<IProps> = ({posts}) => (
-  <div id="blog" className="mb-10 w-11/12 m-auto">
-    <h3 className="text-4xl text-center title font-black">BLOG</h3>
-    <div className="text-center text-lg font-semibold">My last 10 posts</div>
+  <div id="blog" className="w-11/12 m-auto mb-10">
+    <h3 className="text-4xl font-black text-center title">BLOG</h3>
+    <div className="text-lg font-semibold text-center">My last 10 posts</div>
     <section className="text-gray-700">
       <div className="container p-5 mx-auto">
         <div className="flex flex-wrap -m-4">
           {posts.map(post => (
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <a className="block h-48 rounded overflow-hidden hover:opacity-75" href={post.link} target="_blank">
+            <div key={post.guid} className="w-full p-4 lg:w-1/4 md:w-1/2">
+              <a className="block h-48 overflow-hidden rounded hover:opacity-75" href={post.link} target="_blank">
                 <img
                   alt={post.title}
-                  className="object-cover object-center w-full h-full block"
+                  className="block object-cover object-center w-full h-full"
                   src={post.thumbnail}
                   loading="lazy"
                 />
               </a>
               <div className="mt-4">
-                <div className="text-gray-500 text-xs tracking-widest mb-1">
+                <div className="mb-1 text-xs tracking-widest text-gray-500">
                   {post.categories.join(', ').toUpperCase()}
                 </div>
                 <div className="text-lg font-medium">
@@ -42,9 +43,9 @@ const Blog: FC<IProps> = ({posts}) => (
               </div>
             </div>
           ))}
-          <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+          <div className="w-full p-4 lg:w-1/4 md:w-1/2">
             <a
-              className="block h-48 rounded overflow-hidden hover:opacity-75"
+              className="block h-48 overflow-hidden rounded hover:opacity-75"
               href="https://medium.com/@TaishiKato"
               target="_blank">
               <picture>
@@ -52,7 +53,7 @@ const Blog: FC<IProps> = ({posts}) => (
                 <source srcSet="/img/moreposts.png" type="image/jpeg" />
                 <img
                   alt="More posts on Medium"
-                  className="object-cover object-center w-full h-full block"
+                  className="block object-cover object-center w-full h-full"
                   loading="lazy"
                   src="/img/moreposts.png"
                 />
