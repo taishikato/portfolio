a4d1cf8f4694c6cd1a3835195c1dada7765fdc15
taishikato <taishi.k0903@gmail.com>
2022-08-06 12:41:34 -0700

    update OSS css

--- components/Oss.tsx
+++ components/Oss.tsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 const Oss = () => {
   return (
-    <div id="oss" className="m-auto w-10/12 my-10">
-      <h3 className="title font-black text-4xl text-center">OSS Contributions</h3>
-      <ul className="mt-3">
-        <li className="mb-1 font-bold list-disc">
+    <div id="oss" className="w-10/12 m-auto my-10">
+      <h3 className="text-4xl font-black text-center title">OSS Contributions</h3>
+      <ul className="mt-3 text-center">
+        <li className="mb-1 font-bold">
           <a href="https://github.com/nuxt/docs/pull/1799" target="_blank">
             ja: document delete heroku-postbuild section by taishikato · Pull Request #1799 · nuxt/docs
           </a>
         </li>
-        <li className="mb-1 font-bold list-disc">
+        <li className="mb-1 font-bold">
           <a href="https://github.com/nuxt/docs/pull/1797" target="_blank">
             en: document delete heroku-deployment section by taishikato · Pull Request #1797 · nuxt/docs
           </a>
         </li>
-        <li className="mb-1 font-bold list-disc">
+        <li className="mb-1 font-bold">
           <a href="https://github.com/nodejs/node/pull/24589" target="_blank">
-            lib: chenged anonymous function to arrow function by taishikato · Pull Request #24589 · nodejs/node
+            lib: changed anonymous function to arrow function by taishikato · Pull Request #24589 · nodejs/node
           </a>
         </li>
       </ul>
