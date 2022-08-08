f34fbd10ed450b59353bea52e0b8945f771f8788
taishikato <taishi.k0903@gmail.com>
2022-08-08 16:50:53 -0700

    add Marquee component

---
+++ components/Marquee.tsx
@@ -0,0 +1,98 @@
+import {memo} from 'react'
+
+const Marquee = () => {
+  return (
+    <div className="marquee bg-theme h-[50px] mt-20 border-b-2 border-black text-slate-900">
+      <div className="marqueeone">
+        <a href="#" className="text-slate-900">
+          twitter
+        </a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="#" className="text-slate-900">
+          instagram
+        </a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="#" className="text-slate-900">
+          pinterest
+        </a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="#" className="text-slate-900">
+          spotify
+        </a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="#" className="text-slate-900">
+          magazine
+        </a>
+        &nbsp;&nbsp;&nbsp;
+      </div>
+      <div className="marqueetwo">
+        <a href="#" className="text-slate-900">
+          twitter
+        </a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="#" className="text-slate-900">
+          instagram
+        </a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="#" className="text-slate-900">
+          pinterest
+        </a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="#" className="text-slate-900">
+          spotify
+        </a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="#" className="text-slate-900">
+          magazine
+        </a>
+        &nbsp;&nbsp;&nbsp;
+      </div>
+      <div className="marqueethree">
+        <a href="#" className="text-slate-900">
+          twitter
+        </a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="#" className="text-slate-900">
+          instagram
+        </a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="#" className="text-slate-900">
+          pinterest
+        </a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="#" className="text-slate-900">
+          spotify
+        </a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="#" className="text-slate-900">
+          magazine
+        </a>
+        &nbsp;&nbsp;&nbsp;
+      </div>
+      <div className="marqueefour">
+        <a href="#" className="text-slate-900">
+          twitter
+        </a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="#" className="text-slate-900">
+          instagram
+        </a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="#" className="text-slate-900">
+          pinterest
+        </a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="#" className="text-slate-900">
+          spotify
+        </a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="#" className="text-slate-900">
+          magazine
+        </a>
+        &nbsp;&nbsp;&nbsp;
+      </div>
+    </div>
+  )
+}
+
+export default memo(Marquee)
