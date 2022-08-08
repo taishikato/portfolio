6854df5e85dec100b75253d8ba906b3fa12940b0
taishikato <taishi.k0903@gmail.com>
2022-08-08 16:10:35 -0700

    update NewsLetter

--- components/NewsLetter.tsx
+++ components/NewsLetter.tsx
@@ -1,72 +1,76 @@
 const NewsLetter = () => (
-  <div className="border border-slate-600 md:w-96 w-[90%] mx-auto p-3 rounded-md text-slate-100">
-    <p className="text-base">Get notified when I release new stuff.</p>
-    <p className="mb-2">
-      🙏 Subscribed: <span className="font-semibold">84</span>
-    </p>
-    <div id="revue-embed" className="p-3 mx-auto rounded-md">
-      <form
-        action="https://www.getrevue.co/profile/taishik_/add_subscriber"
-        method="post"
-        id="revue-form"
-        name="revue-form"
-        target="_blank"
-        className="flex flex-col gap-y-2">
-        <div className="flex items-center justify-between revue-form-group">
-          <label htmlFor="member_email">Email address</label>
-          <input
-            className="p-2 border rounded-md revue-form-field"
-            placeholder="Your email address..."
-            type="email"
-            name="member[email]"
-            id="member_email"
-          />
-        </div>
-        <div className="flex items-center justify-between revue-form-group">
-          <label htmlFor="member_first_name">
-            First name <span className="optional">(Optional)</span>
-          </label>
-          <input
-            className="p-2 border rounded-md revue-form-field"
-            placeholder="First name... (Optional)"
-            type="text"
-            name="member[first_name]"
-            id="member_first_name"
-          />
-        </div>
-        <div className="flex items-center justify-between revue-form-group">
-          <label htmlFor="member_last_name">
-            Last name <span className="optional">(Optional)</span>
-          </label>
-          <input
-            className="p-2 border rounded-md revue-form-field"
-            placeholder="Last name... (Optional)"
-            type="text"
-            name="member[last_name]"
-            id="member_last_name"
-          />
-        </div>
-        <div className="revue-form-actions">
-          <input
-            type="submit"
-            value="Subscribe"
-            name="member[subscribe]"
-            id="member_submit"
-            className="p-3 font-semibold rounded-md cursor-pointer bg-sky-500 text-slate-200 hover:bg-sky-600"
-          />
-        </div>
-        <div className="revue-form-footer">
-          By subscribing, you agree with Revue’s{' '}
-          <a target="_blank" href="https://www.getrevue.co/terms">
-            Terms of Service
-          </a>{' '}
-          and{' '}
-          <a target="_blank" href="https://www.getrevue.co/privacy">
-            Privacy Policy
-          </a>
-          .
-        </div>
-      </form>
+  <div
+    className="border-2 border-theme md:w-[500px] w-[90%] mx-auto p-6 text-slate-100 font-light"
+    style={{borderRadius: '50% 50% 50% 70%/50% 50% 70% 60%'}}>
+    <div className="w-[80%] mx-auto">
+      <p className="text-base">Get notified when I release new stuff.</p>
+      <p className="mb-2">
+        Subscribed: <span className="font-semibold">84</span>
+      </p>
+      <div id="revue-embed" className="p-3 mx-auto rounded-md">
+        <form
+          action="https://www.getrevue.co/profile/taishik_/add_subscriber"
+          method="post"
+          id="revue-form"
+          name="revue-form"
+          target="_blank"
+          className="flex flex-col gap-y-2">
+          <div className="flex items-center justify-between revue-form-group">
+            <label htmlFor="member_email">Email address</label>
+            <input
+              className="p-2 border rounded-full revue-form-field"
+              placeholder="Your email address..."
+              type="email"
+              name="member[email]"
+              id="member_email"
+            />
+          </div>
+          <div className="flex items-center justify-between revue-form-group">
+            <label htmlFor="member_first_name">
+              First name <span className="optional">(Optional)</span>
+            </label>
+            <input
+              className="p-2 border rounded-full revue-form-field"
+              placeholder="First name... (Optional)"
+              type="text"
+              name="member[first_name]"
+              id="member_first_name"
+            />
+          </div>
+          <div className="flex items-center justify-between revue-form-group">
+            <label htmlFor="member_last_name">
+              Last name <span className="optional">(Optional)</span>
+            </label>
+            <input
+              className="p-2 border rounded-full revue-form-field"
+              placeholder="Last name... (Optional)"
+              type="text"
+              name="member[last_name]"
+              id="member_last_name"
+            />
+          </div>
+          <div className="revue-form-actions">
+            <input
+              type="submit"
+              value="Subscribe"
+              name="member[subscribe]"
+              id="member_submit"
+              className="p-3 font-semibold border-2 rounded-full cursor-pointer border-theme text-slate-200 shadow-box"
+            />
+          </div>
+          <div className="text-sm font-light revue-form-footer">
+            By subscribing, you agree with Revue’s{' '}
+            <a target="_blank" href="https://www.getrevue.co/terms">
+              Terms of Service
+            </a>{' '}
+            and{' '}
+            <a target="_blank" href="https://www.getrevue.co/privacy">
+              Privacy Policy
+            </a>
+            .
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 )
