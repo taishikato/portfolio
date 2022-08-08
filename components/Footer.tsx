70faf74c32ea0e3661eaca2cf414d696a69a40e2
taishikato <taishi.k0903@gmail.com>
2022-08-08 16:50:59 -0700

    add Marquee

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,34 +2,21 @@ import React from 'react'
 import NewsLetter from './NewsLetter'
 import Socials from './Socials'
 import Link from 'next/link'
+import Marquee from './Marquee'
 
 const Footer = () => {
   return (
-    <footer className="px-3 py-20 text-center bg-gray-100">
+    <footer className="px-3 pt-20 text-center border-t border-slate-700">
       <NewsLetter />
-      <p className="mt-4">
-        🙏Much appreciated if you send me $2 on{' '}
-        <a href="https://www.paypal.me/taishikato0903/2usd" target="_blank">
-          PayPal
-        </a>{' '}
-        so I can drink a cup of coffee☕ and create amazingness
-        <br />
-        Follow me on{' '}
-        <a href="https://twitter.com/taishik_" target="_blank">
-          Twitter🐦
-        </a>
-      </p>
-      <p>
-        This challenge is inspired by{' '}
-        <a href="https://www.andreyazimov.com/" target="_blank">
-          Andrey Azimov
-        </a>
-      </p>
-      <Link href="/">
-        <a>taishikato.com</a>
-      </Link>
-      <div className="flex flex-wrap justify-center mt-2 gap-y-3">
+      <Marquee />
+      <div className="pb-6 bg-slate-200 pt-14">
         <Socials />
+        <div className="mt-3 font-light tracking-wide text-slate-800">
+          ©
+          <Link href="/">
+            <a className="text-slate-800">taishikato.com</a>
+          </Link>
+        </div>
       </div>
     </footer>
   )
