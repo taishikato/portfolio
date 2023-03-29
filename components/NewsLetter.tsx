0fb51a7d2516bbdd3ca1ba76d63d9f94283337ef
taishikato <taishi.k0903@gmail.com>
2023-03-29 09:55:09 -0700

    fix title

--- components/NewsLetter.tsx
+++ components/NewsLetter.tsx
@@ -1,6 +1,6 @@
 const NewsLetter = () => (
   <div className="flex flex-col items-center gap-y-2">
-    <p className="text-2xl">Wanna know if my journey?</p>
+    <p className="text-2xl">Wanna know my journey?</p>
     <a
       className="bg-[#FF6E23] px-2 py-3 rounded-full font-bold w-[200px] text-white"
       href="https://taishi.substack.com/"
