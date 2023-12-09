6948765bf1375184ada267f19ad9c428ed8b4f9d
taishikato <taishi.k0903@gmail.com>
2023-12-09 12:14:14 +0700

    Update Home component and remove Blog component import

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,28 +4,29 @@ import TopMain from '../components/TopMain'
 import Skill from '../components/Skill'
 import Oss from '../components/Oss'
 import MyWork from '../components/MyWork'
-import Blog from '../components/Blog'
+// import Blog from '../components/Blog'
 
-const Home = ({posts}) => (
+// const Home = ({posts}) => (
+const Home = () => (
   <div className="bg-slate-50">
     {/* <Hero /> */}
     <TopMain />
     <MyWork />
     <Oss />
     <Skill />
-    <Blog posts={posts} />
+    {/* <Blog posts={posts} /> */}
   </div>
 )
 
-export async function getStaticProps() {
-  const res = await fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@TaishiKato')
-  const posts = await res.json()
+// export async function getStaticProps() {
+//   const res = await fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@TaishiKato')
+//   const posts = await res.json()
 
-  return {
-    props: {
-      posts: posts.items,
-    }, // will be passed to the page component as props
-  }
-}
+//   return {
+//     props: {
+//       posts: posts.items,
+//     }, // will be passed to the page component as props
+//   }
+// }
 
 export default Home
